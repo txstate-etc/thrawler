d6dabc307492c0bdbd41fe8c7f077e11b08ecdc1
Charles Tabor <chastabor@gmail.com>
2016-03-27 10:26:53 -0500

    Alerts for transmogrifiers. Save off thrawler json for debugging

--- reclinks.sh
+++ reclinks.sh
@@ -3,13 +3,18 @@
 
 name="${1:-before}"
 if [ "$name" != 'before' ] && [ "$name" != 'after' ]; then
-	echo "USAGE: $0 [before|after]"
-	exit 1
+  echo "USAGE: $0 [before|after]"
+  exit 1
 fi
 
+# Teeing off json for thrawler debugging.
+# Need to remove cache busting hashes as
+# currently they differ between builds.
 echo 'http://gato-staging-testingsite.its.txstate.edu' |
   ./thrawler --conf=configs/staging-testingsite --threads=8 --proxy='http://localhost' +header='Via: Proxy-HistoryCache/1.8.5' |
+  tee $name.json |
   ./stuc.py |
+  sed 's/magnoliaAssets\/cache[0-9a-z]\+\//magnoliaAssets\/cache...\//g; s/cache[0-9a-z]\+\/imagehandler\//cache...\/imagehandler\//g' |
   sort > $name.txt
 
 # If this is the second phase then diff the
@@ -18,9 +23,16 @@ echo 'http://gato-staging-testingsite.its.txstate.edu' |
 # file; so we do not get any more warnings
 # for the same changes in links.
 if [ $name == 'after' ]; then
-	if ! (diff -U 0 before.txt after.txt >link.diff); then
-		mv after.txt before.txt
-		cat link.diff
-		cat link.diff | mail -s "$(hostname -f) link diffs" "$emails"
-	fi
+  # Find missed transmogrifiers
+  t=$'\t'; grep "$t[^$t]*mjdf38i3tv0b56vz" $name.txt > $name.miss.txt
+	if ! (diff -U 0 before.miss.txt after.miss.txt >links.miss.diff); then
+    mv after.miss.txt before.miss.txt
+    grep -v '^@' links.miss.diff | mail -s "$(hostname -f) missed transmogrifiers" "$emails"
+  fi 
+  if ! (diff -U 0 before.txt after.txt >links.diff); then
+    mv after.txt before.txt
+    grep -v '^@' links.diff | mail -s "$(hostname -f) link diffs" "$emails"
+  fi
+else
+  >$name.miss.txt
 fi
