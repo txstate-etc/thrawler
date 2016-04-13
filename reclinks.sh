a74e2f3e02007ad7167ba60ef3e77eb2fe9630de
Charles Tabor <chastabor@gmail.com>
2016-04-13 12:36:13 -0500

    Swap files at start of run, as opposed to afterwards to give a clearer picture of state when run completes

--- reclinks.sh
+++ reclinks.sh
@@ -2,7 +2,22 @@
 . .env
 
 name="${1:-before}"
-if [ "$name" != 'before' ] && [ "$name" != 'after' ]; then
+# Setup for sequential after passes
+if [ "$name" == 'before' ]; then
+  rm -f after.miss.txt after.link.txt
+  >$name.miss.txt
+elif [ "$name" == 'after' ]; then
+  if [ -f "after.miss.txt" ]; then
+    mv after.miss.txt before.miss.txt
+  else
+    >before.miss.txt
+  fi
+  if [ -f "after.link.txt" ]; then
+    mv after.link.txt before.link.txt
+  else
+    >before.link.txt
+  fi
+else
   echo "USAGE: $0 [before|after]"
   exit 1
 fi
@@ -15,34 +30,29 @@ fi
 curl --user "$magusr" -H 'Accept: application/json' 'http://localhost:8080/mjdf38i3tv0b56vz/.rest/nodes/v1/website/testing-site-destroyer?depth=999&excludeNodeTypes=mgnl:resource,mgnl:metaData,mgnl:content,mgnl:contentNode,mgnl:area,mgnl:component,mgnl:user,mgnl:group,mgnl:role' |
   ./nodes.py -d 'http://gato-staging-testingsite.its.txstate.edu' -s 'testing-site-destroyer' |
   tee $name.node.txt |
-  ./thrawler --conf=configs/gato-staging-testingsite.its.txstate.edu.conf --threads=8 --proxy='http://localhost' --crawl=false +header='Via: Proxy-HistoryCache/1.8.5' |
+  ./thrawler --conf=configs/gato-staging-testingsite.its.txstate.edu.conf --threads=8 --proxy='http://localhost' --crawl=false +header='Via: Proxy-HistoryCache/1.8.5' 2>>./log/thrawler.log |
   tee $name.json |
   ./stuc.py |
-  sed 's/magnoliaAssets\/cache[0-9a-z]\+\//magnoliaAssets\/cache...\//g; s/cache[0-9a-z]\+\/imagehandler\//cache...\/imagehandler\//g' |
-  sort > $name.link.txt
+  sed 's/magnoliaAssets\/cache[0-9a-z]\+\//magnoliaAssets\/cache...\//g; s/cache[0-9a-z]\+\/imagehandler\//cache...\/imagehandler\//g' > $name.link.txt
 
 # If this is the second phase then diff the
 # before and after files. If they differ then
 # overwrite the before file with the after
 # file; so we do not get any more warnings
 # for the same changes in links.
-if [ $name == 'after' ]; then
+if [ $name == 'after' ]; then #after
   # Find missed transmogrifiers
   t=$'\t'; grep "$t[^$t]*mjdf38i3tv0b56vz" $name.link.txt > $name.miss.txt
   ./parity before.miss.txt after.miss.txt >links.miss.diff
   if [ -s "links.miss.diff" ]; then
     echo '========== Missed Transmogrified Links =========='
     cat links.miss.diff
-    mv after.miss.txt before.miss.txt
     cat links.miss.diff | mail -s "thrawler missed transmogrifiers $(hostname -f)" "$emails"
   fi
-  ./parity before.link.txt after.link.txt > links.diff
+  ./parity before.link.txt after.link.txt >links.diff
   if [ -s "links.diff" ]; then
     echo '========== Differing Links =========='
     cat links.diff
-    mv after.link.txt before.link.txt
     cat links.diff | mail -s "thrawler link diffs $(hostname -f)" "$emails"
   fi
-else
-  >$name.miss.txt
 fi
